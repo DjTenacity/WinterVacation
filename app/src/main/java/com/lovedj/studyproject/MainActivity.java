dc0fdd53cfb9bdf97a42e6f030f658db8f5765b7
高殿杰 <1757286697@qq.com>
2018-03-28 22:34:59 +0800

    EventBus的削弱版自定义 学习

--- app/src/main/java/com/lovedj/studyproject/MainActivity.java
+++ app/src/main/java/com/lovedj/studyproject/MainActivity.java
@@ -1,32 +1,76 @@
 package com.lovedj.studyproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.lovedj.studyproject.R;
+import com.lovedj.studyproject.customview.LoveLayout;
+import com.lovedj.studyproject.eventbus.EventBus;
+import com.lovedj.studyproject.eventbus.Subscribe;
+import com.lovedj.studyproject.eventbus.TestActivity;
+import com.lovedj.studyproject.eventbus.ThreadMode;
+
 public class MainActivity extends AppCompatActivity {
 
-   // static {System.loadLibrary("native-lib");}
+    // static {System.loadLibrary("native-lib");}
+
+    private TextView mTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 注册，思考为什么要注册？
+        EventBus.getDefault().register(this);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-
+        // 进入测试界面
+        mTv = (TextView) findViewById(R.id.test_tv);
+        mTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
+     * threadMode 执行的线程方式
+     * priority 执行的优先级
+     * sticky 粘性事件
      */
-   // public native String stringFromJNI();
+    @Subscribe(threadMode = ThreadMode.MAIN, priority = 50, sticky = true)
+    public void test1(String msg) {
+        // 如果有一个地方用 EventBus 发送一个 String 对象，那么这个方法就会被执行
+        Log.e("TAG", "msg1 = " + msg);
+        mTv.setText(msg);
+    }
+
+    /**
+     * threadMode 执行的线程方式
+     * priority 执行的优先级，值越大优先级越高
+     * sticky 粘性事件
+     */
+    @Subscribe(threadMode = ThreadMode.MAIN, priority = 100, sticky = true)
+    public void test2(String msg) {
+        // 如果有一个地方用 EventBus 发送一个 String 对象，那么这个方法就会被执行
+        Log.e("TAG", "msg2 = " + msg);
+        mTv.setText(msg);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // 解绑，思考为什么要解绑？
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
+    }
 
     public void addLove(View view) {
-        LoveLayout love_layout =findViewById(R.id.love_layout);
+        LoveLayout love_layout = findViewById(R.id.love_layout);
         love_layout.addLove();
     }
 }
