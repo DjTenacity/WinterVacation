5fdd31c0030f7dcd8db4dab573b61080a6ed5e74
高殿杰 <1757286697@qq.com>
2018-04-22 11:27:20 +0800

    okhttp 流程梳理

--- app/src/main/java/com/lovedj/studyproject/okhttp/OkHttpActivity.java
+++ app/src/main/java/com/lovedj/studyproject/okhttp/OkHttpActivity.java
@@ -16,6 +16,11 @@ public class OkHttpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ok_http);
 
 
+        //把request传入OkHttpClient,返回一个实现了Call的RealCall对象,
+        //然后这个对象调用了enqueue(new Callback)方法,在这个enqueue方法里面就会创建一个异步的实现了 Runnable的AsyncCall对象,并传入我们创建的CallBack对象
+        //然后把这个asyncCall 对象传给 okhttpClient的dispatch,添加到线程池里面轮询
+        //值得注意的是  添加拦截器进行网络请求都是在这个AsyncCall对象里面execute()方法进行的
+
         OkHttpClient client = new OkHttpClient();
         RequestBody requestBody = new RequestBody()
                 .type("")
